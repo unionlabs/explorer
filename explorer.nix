f608992debd1ffb4692148e3324c80d54ae8d687
PoisonPhang <17688291+PoisonPhang@users.noreply.github.com>
2025-08-22 11:55:56 -0500

    feat: nixify

---
+++ explorer.nix
@@ -0,0 +1,52 @@
+{ ... }: {
+  perSystem = { pkgs, self', system, ... }: {
+    packages = {
+      explorer =
+      let
+        offlineCache = pkgs.fetchYarnDeps {
+          yarnLock = ./yarn.lock;
+          hash = "sha256-Na1KQbIlD0aRGIW3hYW6eCXMLqFziarYFIdcKMVNL+k=";
+        };
+      in
+      pkgs.stdenv.mkDerivation {
+        pname = "pingpub-explorer";
+        version = "0.1.0";
+        src = builtins.filterSource 
+          (path: type: 
+            type != "directory" || baseNameOf path != ".github") 
+          (pkgs.lib.cleanSource ./.);
+
+        nativeBuildInputs = with pkgs; [
+          tree
+          fixup-yarn-lock
+          nodePackages_latest.yarn
+          nodePackages_latest.nodejs
+        ];
+
+        configurePhase = ''
+          runHook preConfigure
+
+          export HOME=$(mktemp -d)
+
+          yarn config --offline set yarn-offline-mirror "${offlineCache}"
+          fixup-yarn-lock yarn.lock
+          yarn install --offline --frozen-lockfile --ignore-platform --ignore-scripts --no-progress --non-interactive
+          export PATH="$PATH:node_modules/vite/bin"
+
+          patchShebangs node_modules/
+
+          runHook postConfigure
+        '';
+
+        buildPhase = ''
+          ./node_modules/vite/bin/vite.js build
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          mv dist/* $out/
+        '';
+      };
+    };
+  };
+}
